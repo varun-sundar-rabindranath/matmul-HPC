9afe1a91ad33125508c110077f7646bd7e3c11a5
Varun Sundar <varunsundar08@gmail.com>
2016-12-08 19:46:39 +0530

    Add utilitites

---
+++ utils.hpp
@@ -0,0 +1,17 @@
+#ifndef __UTILS_HPP__
+#define __UTILS_HPP__
+
+void print_matrix(int* mat, int dim, char* comment = NULL) {
+
+  if(comment)
+    cout<<comment<<endl;
+
+  for (int y = 0; y < dim; y++) {
+    for (int x = 0; x < dim; x++) {
+      cout<<mat[y * dim + x]<<" ";
+    }
+    cout<<endl;
+  }
+}
+
+#endif // __UTILS_HPP__
